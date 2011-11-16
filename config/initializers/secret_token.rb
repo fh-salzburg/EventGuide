a9e462f89ea347b4e290df59994ef03e34741ba9
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2011-11-16 12:59:35 +0100

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventGuide::Application.config.secret_token = '7a2e28b77ad151a1ff373fd2465aa00c0a93fe69d0822c926ef6a2330e13b6f91e6d1db795ebc59a597bec86d6c8a2df40b2c28db41e04ff79b6390857a205ff'
