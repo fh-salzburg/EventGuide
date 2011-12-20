d52eabef2901ed6e1caf1ae4cd9bf73823396f08
Michael Tiefenthaler <michael.tiefenthaler@gmx.at>
2011-12-20 11:07:44 +0100

    added render menu to index.htm.erb of events/

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111206175439) do
+ActiveRecord::Schema.define(:version => 20111219230944) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(:version => 20111206175439) do
     t.string   "image"
     t.boolean  "isAQuiz"
     t.time     "atTime"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "menus", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
