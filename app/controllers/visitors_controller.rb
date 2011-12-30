a990169ba1e0a59a94fa26ca3fcb0b707e2fd610
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2011-12-30 18:54:22 +0100

    forms modified, visitors controller actions added

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,23 @@
 class VisitorsController < BaseController
+
   def index
   end
+
+  def new
+    @visitor = Visitor.new
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
   
 end
