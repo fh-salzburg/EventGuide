af804a13fade52292e55012ec77a3bbfa6efff24
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2012-01-02 14:23:01 +0100

    event model modified

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
-  attr_accessible :name, :room, :time_from, :time_to, :isAStation, :description, :short_description, :image
+  attr_accessible :title, :room, :time_from, :time_to, :isAStation, :description, :short_description, :image
   
-  validates_presence_of :name, :on => :create, :message => "angeben"
+  validates_presence_of :title, :on => :create, :message => "angeben"
   validates_presence_of :room, :on => :create, :message => "angeben"
   validates_presence_of :isAStation, :on => :create, :message => "angeben"
   validates_presence_of :description, :on => :create, :message => "angeben"
