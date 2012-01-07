9329f3f1a540823eb1608d20e1d220db96613cd5
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2012-01-07 15:00:25 +0100

    helper for permissions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,21 @@ module ApplicationHelper
       time.strftime("%d.%m.%Y %H:%M")
     end
   end
+  
+  def is_admin
+    if @permission == "admin"
+    return true
+    else
+    return false
+    end
+  end
+
+  def is_guide
+    if @permission == "guide" || @permission == "admin"
+    return true
+    else
+    return false
+    end
+  end
+
 end
