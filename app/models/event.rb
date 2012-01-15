ca2433c2407849f6b3c307d950d75501598c8523
Michael Tiefenthaler <michael.tiefenthaler@gmx.at>
2012-01-15 15:24:28 +0100

    valiations für bilder hinzugefügt

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,9 +13,9 @@ class Event < ActiveRecord::Base
                     :url  => "/assets/images/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension"
 
-  #validates_attachment_presence :photo
-  #validates_attachment_size :photo, :less_than => 5.megabytes
-  #validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
   validates_presence_of :title, :on => :create, :message => "angeben"
   validates_presence_of :room, :on => :create, :message => "angeben"
   validates_presence_of :description, :on => :create, :message => "angeben"
