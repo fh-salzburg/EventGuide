f65abd44b1d2231bd66ac2bd5ae070bc2ccd0a32
Michael Tiefenthaler <mtiefenthaler.mmt-b2010@fh-salzburg.ac.at>
2012-03-12 23:20:36 +0100

    Browseranpassung

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.authenticate(params[:email], params[:password])
+      
     if user
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Login erfolgreich"
